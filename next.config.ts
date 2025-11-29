22ba131f825f4fb02685b7d97f2c67eb86fa3738
dyuanna <annadyu666@gmail.com>
2025-11-29 17:33:11 +0900

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/next.js1",
 };
 
 export default nextConfig;
